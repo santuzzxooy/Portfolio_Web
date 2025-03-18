2b1d2da075854f6b7fa98ffdc982e95fddc8c4e1
santuzzxooy <santiagorochav@gmail.com>
2025-03-18 00:31:21 -0500

    mejoritas

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,31 @@ import ValoresCard from './Components/ValoresCard'
 import StudiesCard from './Components/StudiesCard'
 import Footer from './Components/Footer'
 import ScrollToTop from './Components/ScrollToTop'
+import { FaBolt } from "react-icons/fa";
 import { RefsProvider } from "./utils/RefsContext";
+import { useState, useEffect } from "react";
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      {loading ? (
+        <div className="full-page-spinner">
+          <div className="spinner"></div>
+          <p className="loading-text">Loading...</p>
+          <FaBolt className="loading-icon" />
+        </div>
+      ) : (
       <RefsProvider>
         <Header />
         <Banner />
@@ -23,6 +42,7 @@ function App() {
         <ScrollToTop />
         <Footer />
       </RefsProvider>
+      )}
     </>
   )
 }
